aaed67edccaabfb4014735872e09d7503522d1c4
feifei <feifei@qq.com>
2017-10-29 22:48:41 +0800

    下班咯

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,3 +1,23 @@
 /**
  * Created by F on 2017/10/29.
  */
+//希望在ajax调用之前start
+//在ajax调用结束后执行done
+$(function () {
+  
+  
+  
+  //让进度条显示
+  $(document).ajaxStart(function () {
+    //让进度条显示出来
+    NProgress.start();
+  })
+  
+  
+  $(document).ajaxStop(function () {
+    setTimeout(function () {
+      //让进度条结束
+      NProgress.done();
+    }, 500);
+  });
+});
